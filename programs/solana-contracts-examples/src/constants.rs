79fd5d8f9d4d956085ccd0bb9fda4bc89408d7ea
Osman Abdelnasir <esaminu1@icloud.com>
2025-07-04 04:17:37 +0400

    feat: add erc20 deposit and claim

--- programs/solana-contracts-examples/src/constants.rs
+++ programs/solana-contracts-examples/src/constants.rs
@@ -1,5 +1,5 @@
 // Chain Signatures Program ID on devnet
-pub const CHAIN_SIGNATURES_PROGRAM_ID: &str = "BtGZEs9ZJX3hAQuY5er8iyWrGsrPRZYupEtVSS129XKo";
+pub const CHAIN_SIGNATURES_PROGRAM_ID: &str = "4uvZW8K4g4jBg7dzPNbb9XDxJLFBK7V6iC76uofmYvEU";
 
 // Chain signatures program state seeds
 pub const CHAIN_SIGNATURES_STATE_SEED: &[u8] = b"program-state";

2568da14bef687d0c7be16ef2c05e6b940d28f88
Pessina <fs.pessina@gmail.com>
2025-07-20 15:26:56 +0500

    Implement balance-box

--- frontend/components/balance-display/balance-box.tsx
+++ frontend/components/balance-display/balance-box.tsx
@@ -19,89 +19,26 @@ export function BalanceBox({
 }) {
   return (
     <div
-      className={cn('flex items-center border-t border-[#A28B8A]', className)}
-      style={{
-        gap: '67px',
-        padding: '20px 0px',
-        width: '100%',
-      }}
+      className={cn(
+        'flex items-center justify-between py-5 border-t border-neutral-400 w-full',
+        className,
+      )}
     >
-      <div
-        className='flex'
-        style={{
-          flexDirection: 'row',
-          gap: '10px',
-          width: '176px',
-        }}
-      >
-        <div
-          className='flex flex-col'
-          style={{
-            gap: '7px',
-            width: '100px',
-          }}
-        >
-          <div
-            style={{
-              fontFamily:
-                'Test Söhne Mono, ui-monospace, SFMono-Regular, Consolas, monospace',
-              fontWeight: 300,
-              fontSize: '32px',
-              lineHeight: '1.5em',
-              textAlign: 'left',
-              color: '#6C6060',
-            }}
-          >
-            {amount}
-          </div>
-
-          <div
-            style={{
-              fontFamily:
-                'Test Söhne Mono, ui-monospace, SFMono-Regular, Consolas, monospace',
-              fontWeight: 600,
-              fontSize: '14px',
-              lineHeight: '1.5em',
-              letterSpacing: '-0.01em',
-              textAlign: 'left',
-              color: '#9C8686',
-            }}
-          >
+      <div className='flex gap-8'>
+        <div className='flex flex-col gap-2'>
+          <div className='text-3xl font-light text-text-primary'>{amount}</div>
+          <div className='text-sm font-semibold text-text-secondary'>
             {usdValue}
           </div>
         </div>
-
-        <div
-          className='flex items-center'
-          style={{
-            gap: '6px',
-          }}
-        >
+        <div className='flex items-center gap-4'>
           {icon}
-          <span
-            style={{
-              fontFamily: 'Elza Text, system-ui, sans-serif',
-              fontWeight: 700,
-              fontSize: '16px',
-              lineHeight: '1.5em',
-              letterSpacing: '-0.01em',
-              textAlign: 'left',
-              color: '#6C6060',
-            }}
-          >
+          <span className='text-base font-bold text-text-primary'>
             {tokenSymbol}
           </span>
         </div>
       </div>
-
-      <div
-        className='flex'
-        style={{
-          gap: '20px',
-        }}
-      >
-        <ActionButtons />
-      </div>
+      <ActionButtons />
     </div>
   );
 }
